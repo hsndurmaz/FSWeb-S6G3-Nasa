7834d9524ff820e1ab32288d409d391a2ed1e99b
hsndurmaz <hsndurmaz3@gmail.com>
2023-07-05 23:56:13 +0300

    tamamlanmadı

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [apod, , setApod] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov.planetary/apod?api_key=F18Xf0DnbtolzZfU6AtLZxCSnAYlDtdNY35MaTsI"
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {});
+  }, []);
+
   return (
     <div className="App">
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        <span role="img" aria-label="go!">
+          {!Object.keys(apod).length && <p>Yükleniyor...</p>}
+          <div>
+            <h1>{apod.title}</h1>
+            <span>{apod.date}</span>
+            <p>{apod.explanation} </p>
+
+            <img src={apod.url} alt={apod.title} />
+          </div>
+        </span>
       </p>
     </div>
   );
